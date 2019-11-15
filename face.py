2fe18cfa48d45f5c850487aa41b04041bdec1586
Rafael <rd17647@bristol.ac.uk>
2019-11-15 13:16:40 +0000

    started on gridsearch

--- face.py
+++ face.py
@@ -1,6 +1,11 @@
 import numpy as np
 import cv2 as cv
 import argparse
+import operator
+import matplotlib.pyplot as plt 
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
 
 from eval import eval
 
@@ -20,12 +25,12 @@ face_cascade = cv.CascadeClassifier('frontalface.xml')
 
 
 #detects image and returns array called 'faces' with subarrays containg x, y, width and height of all boxes around detected faces.
-def detect(path, image):
-    print(image)
+def detect(path, image, scaleFactor=1.1, minNeighbors=1.1):
     img = cv.imread(path)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     gray = cv.equalizeHist(gray)
-    faces = np.asarray(face_cascade.detectMultiScale(image=gray, scaleFactor=1.1, minNeighbors=1, minSize=(50, 50), maxSize=(500,500)))
+    faces = np.asarray(face_cascade.detectMultiScale(image=gray, scaleFactor=scaleFactor,
+     minNeighbors=minNeighbors, minSize=(50, 50), maxSize=(500,500)))
 
 # highlights regions of interest and draws them onto the image.
     for (x,y,w,h) in faces:
@@ -38,10 +43,43 @@ def detect(path, image):
         roi_color = img[y:y+h, x:x+w]
     faces_count = faces.shape[0]
 #displays the image with roi
-    print(eval(groundTruths[image], faces))
     cv.imwrite('detected.jpg',img)
+    return groundTruths[image], faces
 
 
-images=('dart4.jpg','dart5.jpg','dart13.jpg','dart14.jpg','dart15.jpg')
-for i in images:
-    detect('images/positives/'+i, i)
+
+def benchmark (scaleFactor=1.1, minNeighbors=1):
+    P, R = [], []
+    for k in groundTruths.keys():
+        targets, predicts = detect('images/positives/'+k, k, scaleFactor, minNeighbors)
+        p, r, f1 = eval(targets, predicts)
+        P.append(p)
+        R.append(r)
+        # print(k, p, r, f1)
+    AP = np.mean(P)
+    AR = np.mean(R)
+    AF1 = 2 * ((AP*AR)/ (AP + AR))
+    # print("average", AP, AR, AF1)
+    return AP, AR, AF1
+
+def gridsearch ():
+    SF, MN = [], []
+    for sf in np.linspace(1.1, 10, 11):
+        _ , _ , f = benchmark(scaleFactor=sf)
+        SF.append(f)
+    for mn in range (1, 10):
+        _ , _ , f = benchmark(scaleFactor=1.1, minNeighbors=mn)
+        MN.append(f)
+    print(max(SF), np.argmax(SF),1+(0.1*np.argmax(SF)))
+    fig, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
+    ax1.plot(np.linspace(1.1,10,11), np.asarray(SF))
+    ax2.plot(np.arange(1, 10, 1), np.asarray(MN))
+    plt.show()
+
+    return opt, config[opt]
+
+            
+
+#print(detect('images/positives/'+args.image, args.image))
+print(benchmark(4.7, 3))
+# print(gridsearch())
